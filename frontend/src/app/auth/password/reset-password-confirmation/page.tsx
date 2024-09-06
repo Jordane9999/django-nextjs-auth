236d2d2ba5e26a7843c94434f0aa3c6b39fa9750
Jordane9999 <kpadejordane@gmail.com>
2024-09-06 02:58:55 +0200

    ajout des amelioration du frontend

---
+++ frontend/src/app/auth/password/reset-password-confirmation/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ResetPasswordConfirmationPage() {
+  return <div>ResetPasswordConfirmation</div>;
+}
---
+++ frontend/src/app/components/ResetPasswordConfirmation.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ResetPasswordConfirmation() {
+  return <div>ResetPasswordConfirmation</div>;
+}
